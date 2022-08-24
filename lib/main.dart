8a1a519673e50b946d4a42c476a8ba7152a9622c
Alex <AlexeyZ213@yandex.ru>
2022-08-24 14:24:57 +0300

    main to functional widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:food_down_menu/menu_bar.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+
+part 'main.g.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Simple Animations',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+@swidget
+Widget myApp() {
+  return MaterialApp(
+    title: 'Flutter Simple Animations',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const Scaffold(
+      body: Center(
+        child: MenuBar(),
       ),
-      home: const Scaffold(
-        body: Center(
-          child: MenuBar(),
-        ),
-      ),
-    );
-  }
+    ),
+  );
 }
